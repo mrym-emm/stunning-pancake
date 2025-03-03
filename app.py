f85de7c004050fcde831192a08ed25f461c87fd4
mrym-emm <153648180+mrym-emm@users.noreply.github.com>
2025-03-03 09:46:58 +0000

    added fav icon. cute

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # an app is simply an object of the class Flask
 app = Flask(__name__)
@@ -6,8 +6,15 @@ app = Flask(__name__)
 # everything after the domain name (github/) is the path or route
 # so if someone goes to the homepage, aythign below the decorator will run
 @app.route("/")
-def hello_world():
-    return "Hello, mrym!"
+def hello_pancakes():
+  return render_template("home.html") # this will render the html file
+
+ 
+
+
+
+# def hello_world():
+#     return "Hello, mrym!"
 # def add_numbers():
 #   return str(1+2) # can only be strings apparently
 
