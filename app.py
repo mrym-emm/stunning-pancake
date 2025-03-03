a1025d28a764ee79713a6831109b8478dc543f17
mrym-emm <153648180+mrym-emm@users.noreply.github.com>
2025-03-03 18:02:04 +0000

    dynamic data. adding list of pancake places to flask and rendering

--- app.py
+++ app.py
@@ -3,11 +3,34 @@ from flask import Flask, render_template
 # an app is simply an object of the class Flask
 app = Flask(__name__)
 
+PLACES = [
+  # making dictionary inside of list
+  # to make this appear on website, pass it tru the render website
+  {'id': 1,
+   'name': "The Nook",
+   'location': "Tamarind Square",
+   'price_range': "RM10"
+  },
+
+  {'id': 2,
+   'name': "Soufflé Dessert Cafe",
+   'location': "Damansara Utama",
+   'price_range': "RM18"
+  },
+
+  {'id': 3,
+   'name': "Flufff",
+   'location': "Ipoh"
+  }
+]
+
+
 # everything after the domain name (github/) is the path or route
 # so if someone goes to the homepage, aythign below the decorator will run
 @app.route("/")
 def hello_pancakes():
-  return render_template("home.html") # this will render the html file
+  return render_template("home.html", 
+                         places = PLACES) # this will render the html file
 
  
 
